39e2fe234fa6369939d9d0eb7077a20b6d9adbb3
Charlotte Som <charlotte@som.codes>
2025-06-13 13:04:23 +0100

    export inferenceSymbol

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,6 +1,8 @@
 export * from "./compile.ts";
 export * from "./schema.ts";
 
+export { out as inferenceSymbol } from "./_internal.ts";
+
 export * from "./schema/array.ts";
 export * from "./schema/basic.ts";
 export * from "./schema/custom.ts";
