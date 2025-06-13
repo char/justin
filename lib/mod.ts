a2e13461d0a779da6175a15ca7587af36e1a95ab
Charlotte Som <charlotte@som.codes>
2025-06-13 11:08:59 +0100

    0.3.0: discriminated unions

--- lib/mod.ts
+++ lib/mod.ts
@@ -4,6 +4,7 @@ export * from "./schema.ts";
 export * from "./schema/array.ts";
 export * from "./schema/basic.ts";
 export * from "./schema/custom.ts";
+export * from "./schema/discriminated-union.ts";
 export * from "./schema/literal.ts";
 export * from "./schema/object.ts";
 export * from "./schema/optional.ts";
---
+++ lib/schema/discriminated-union.ts
@@ -0,0 +1,113 @@
+import {
+  compileSchema,
+  concatIR,
+  irEmitError,
+  irError,
+  irNext,
+  irValue,
+  registerSchemaCompiler,
+  type IREntry,
+  type SchemaCompiler,
+} from "../_compile_internal.ts";
+import type { out } from "../_internal.ts";
+import type { Flatten, TBox } from "../_util.ts";
+import type { AnySchema, Infer } from "../schema.ts";
+import type { LiteralSchema } from "./literal.ts";
+import type { ObjectSchema } from "./object.ts";
+
+type Values<T> = Flatten<T[keyof T]>;
+export interface DiscriminatedUnionSchema<
+  InSchemas extends readonly ObjectSchema<Record<string, AnySchema>>[],
+> {
+  readonly type: "d-union";
+  readonly discriminant: string;
+  readonly schemas: InSchemas;
+
+  /** @ignore */
+  // prettier-ignore
+  readonly [out]?: TBox<Values<{
+    [K in keyof InSchemas as
+      K extends `${infer N extends number}` ? N : never
+    ]: Infer<InSchemas[K]>;
+  }>>;
+}
+
+const compileDiscriminatedUnion: SchemaCompiler<
+  DiscriminatedUnionSchema<readonly ObjectSchema<Record<string, AnySchema>>[]>
+> = (ctx, schema) => {
+  const suberrors = ctx.locals.next();
+  const suberror = ctx.locals.next();
+
+  let subschemaValidation: IREntry[] = [irNext];
+  for (const subschema of schema.schemas) {
+    const discriminant = subschema.shape[schema.discriminant] as LiteralSchema<any>;
+    if (discriminant.type !== "literal") {
+      throw new Error("union discriminant must be a LiteralSchema");
+    }
+
+    const step = [];
+    for (let i = 0; i < subschemaValidation.length; i++) {
+      const entry = subschemaValidation[i];
+      if (entry === irNext) {
+        // step.push(
+        //   ...concatIR`${valid} = true;
+        //   ${compileSchema(ctx, subschema).flatMap((it) =>
+        //     it === irError
+        //       ? [invalidate]
+        //       : it === irNext
+        //         ? concatIR`if (${valid}) break; ${irNext}`
+        //         : [it],
+        //   )}`,
+        // );
+
+        step.push(
+          ...concatIR`case ${JSON.stringify(discriminant.value)}: {
+          const ${suberrors} = [];
+          ${compileSchema(ctx, subschema).flatMap((it) => (it === irNext ? [] : it === irError ? [suberrors, ".push"] : it))}
+          for (const ${suberror} of ${suberrors}) {
+            ${irError}(${suberror});
+          }
+          break;
+        }
+        ${irNext}`,
+        );
+      } else {
+        step.push(entry);
+      }
+    }
+    subschemaValidation = step;
+  }
+
+  return concatIR`switch (${irValue}[${JSON.stringify(schema.discriminant)}]) {
+    ${subschemaValidation}
+    default: {
+      ${irEmitError({ ...ctx, path: ctx.path + "." }, "unknown union variant")};
+      break;
+    }
+  }
+  ${irNext}`;
+};
+
+/**
+ * a discriminated union of object values.
+ *
+ * compiles to a `switch` statement, so the discriminant must be a `j.literal` in each case.
+ *
+ * ```typescript
+ * const Schema = j.discriminatedUnion("t", [
+ *   j.obj({ t: j.literal("1"), a: j.string }),
+ *   j.obj({ t: j.literal("2"), b: j.string }),
+ *   j.obj({ t: j.literal("3"), c: j.string }),
+ * ]);
+ * ```
+ */
+export const discriminatedUnion: <
+  const InSchemas extends readonly ObjectSchema<Record<string, AnySchema>>[],
+>(
+  discriminant: string,
+  schemas: InSchemas,
+) => DiscriminatedUnionSchema<InSchemas> = /* #__PURE__ */ registerSchemaCompiler(
+  "d-union",
+  compileDiscriminatedUnion,
+  (discriminant, schemas) => ({ type: "d-union", discriminant, schemas }),
+);
